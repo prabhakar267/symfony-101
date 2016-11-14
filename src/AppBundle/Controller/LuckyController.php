28d5107f60ab8d654c03279fb95c48782a8f1549
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-11-15 00:17:48 +0530

    slug accepted + redirection

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -9,13 +9,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class LuckyController extends Controller
 {
     /**
-     * @Route("/lucky/number")
+     * @Route("/lucky/{number}", name="lucky_number")
      */
-    public function numberAction()
+    public function numberAction($number)
     {
-        $number = mt_rand(0, 100);
+        // $number = mt_rand(0, 100);
 
         return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
+    }
+
+    /**
+     * @Route("/lucky", name="lucky_number_generate")
+     */
+    public function emptyNumberAction()
+    {
+        // if the user doesnt enter a number himself
+        // he will be redirected to a number page
+        $number = mt_rand(0, 100);
+
+        return $this->redirectToRoute('lucky_number', array(
             'number' => $number,
         ));
     }
